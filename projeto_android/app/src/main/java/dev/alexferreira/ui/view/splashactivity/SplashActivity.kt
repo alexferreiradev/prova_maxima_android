567fc0d9ecd0cc6dbef38c83c6e3a0e9ff128aad
Alex Rabelo Ferreira <arf92@live.com>
2019-09-10 12:56:38 -0300

    Adicao de arquivos de projeto Android

---
+++ projeto_android/app/src/main/java/dev/alexferreira/ui/view/splashactivity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package dev.alexferreira.ui.view.splashactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
