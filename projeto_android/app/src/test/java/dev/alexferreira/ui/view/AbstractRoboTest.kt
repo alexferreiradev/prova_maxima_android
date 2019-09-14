8e252913fa1efe6718652ce28b8268e091263c80
Alex Rabelo Ferreira <arf92@live.com>
2019-09-14 19:14:01 -0300

    Correcoes de splash test para uso de abstract test

--- projeto_android/app/src/test/java/dev/alexferreira/ui/view/AbstractRoboTest.kt
+++ projeto_android/app/src/test/java/dev/alexferreira/ui/view/AbstractRoboTest.kt
@@ -6,26 +6,57 @@ import dev.alexferreira.application.RoboApp
 import dev.alexferreira.ui.contract.IPresenter
 import dev.alexferreira.ui.contract.IView
 import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = RoboApp::class)
 abstract class AbstractRoboTest<A : AbstractView<V, P>, V : IView, P : IPresenter<V>>(private val actClass: Class<A>) {
 
-    private lateinit var intent: Intent
-    private lateinit var context: Context
+    protected lateinit var intent: Intent
+    protected lateinit var context: Context
 
-    private lateinit var presenter: P
-    private lateinit var activity: A
+    protected lateinit var presenter: P
+    protected lateinit var activity: A
+    protected lateinit var view: V
 
+    @Suppress("UNCHECKED_CAST")
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         activity = Robolectric.buildActivity(actClass).create().start().get()
         presenter = activity.presenter
+        context = RuntimeEnvironment.systemContext
+        view = activity as V
+    }
+
+    @Test
+    fun whenCreate_callPresenter() {
+        Mockito.verify(presenter).onViewCreated(view, activity.applicationContext, activity.intent)
+    }
+
+    @Ignore("Não consegui fazer mock de destroy e pause")
+    @Test
+    fun onViewDestroyed_callPresenter() {
+        Mockito.verify(presenter).onViewDestroyed()
+    }
+
+    @Ignore("Não consegui fazer mock de destroy e pause")
+    @Test
+    fun onViewPaused_callPresenter() {
+        activity.finish()
+        Mockito.verify(presenter).onViewPaused()
+    }
+
+    @Test
+    fun onViewStarted_callPresenter() {
+        Mockito.verify(presenter).onViewStarted(activity.intent)
     }
 }
--- projeto_android/app/src/test/java/dev/alexferreira/ui/view/SplashActivityRoboTest.kt
+++ projeto_android/app/src/test/java/dev/alexferreira/ui/view/SplashActivityRoboTest.kt
@@ -1,69 +1,24 @@
 package dev.alexferreira.ui.view
 
-import android.content.Context
-import android.content.Intent
 import android.widget.ImageView
 import dev.alexferreira.R
 import dev.alexferreira.application.RoboApp
 import dev.alexferreira.ui.contract.SplashContract
 import org.junit.Assert.assertNotNull
-import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = RoboApp::class)
-class SplashActivityRoboTest {
-    private lateinit var intent: Intent
-    @Mock
-    private lateinit var context: Context
-
-    private lateinit var presenter: SplashContract.Presenter
-    private lateinit var activity: SplashActivity
-
-    @Before
-    fun setUp() {
-        MockitoAnnotations.initMocks(this)
-        activity = Robolectric.buildActivity(SplashActivity::class.java).create().start().get()
-        presenter = activity.presenter
-    }
+class SplashActivityRoboTest :
+    AbstractRoboTest<SplashActivity, SplashContract.View, SplashContract.Presenter>(SplashActivity::class.java) {
 
     @Test
     fun whenStart_showAppBackground() {
         val image = activity.findViewById<ImageView>(R.id.iv_background)
 
         assertNotNull(image)
-    }
-
-//    Teste de Abstract View +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-    @Test
-    fun whenCreate_callPresenter() {
-        Mockito.verify(presenter).onViewCreated(activity, activity.applicationContext, activity.intent)
-    }
-
-    @Ignore("Não consegui fazer mock de destroy e pause")
-    @Test
-    fun onViewDestroyed_callPresenter() {
-        Mockito.verify(presenter).onViewDestroyed()
-    }
-
-    @Ignore("Não consegui fazer mock de destroy e pause")
-    @Test
-    fun onViewPaused_callPresenter() {
-        activity.finish()
-        Mockito.verify(presenter).onViewPaused()
-    }
-
-    @Test
-    fun onViewStarted_callPresenter() {
-        Mockito.verify(presenter).onViewStarted(activity.intent)
     }
 }
