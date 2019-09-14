be89dbf79488eefce98f273ac92f7cea2c24dcda
Alex Rabelo Ferreira <arf92@live.com>
2019-09-14 18:31:26 -0300

    Adicao de layouts de todas telas e items de listas

---
+++ projeto_android/app/src/main/java/dev/alexferreira/ui/view/HistoricoPedidoActivity.kt
@@ -0,0 +1,13 @@
+package dev.alexferreira.ui.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import dev.alexferreira.R
+
+class HistoricoPedidoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historico_pedido)
+    }
+}
