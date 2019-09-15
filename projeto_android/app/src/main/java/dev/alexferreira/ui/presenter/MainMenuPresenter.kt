8e02ff113b6464d6fdf7594d7f9ab1d380fd2b62
Alex Rabelo Ferreira <arf92@live.com>
2019-09-15 00:05:01 -0300

    Implementacao de mainMenu presenter

--- projeto_android/app/src/main/java/dev/alexferreira/ui/presenter/MainMenuPresenter.kt
+++ projeto_android/app/src/main/java/dev/alexferreira/ui/presenter/MainMenuPresenter.kt
@@ -1,13 +1,26 @@
 package dev.alexferreira.ui.presenter
 
+import dev.alexferreira.R
+import dev.alexferreira.data.model.Cliente
 import dev.alexferreira.injection.scope.ActivityScope
 import dev.alexferreira.ui.contract.MainMenuContract
+import dev.alexferreira.ui.model.MainMenuModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @ActivityScope
 class MainMenuPresenter @Inject constructor() : AbstractPresenter<MainMenuContract.View>(), MainMenuContract.Presenter {
 
-    override fun selectMenuItem() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    lateinit var currentCliente: Cliente
+
+    override fun selectMenuItem(menuItem: MainMenuModel) {
+        when (menuItem.title) {
+            context.getString(R.string.dados_cliente_main_menu_title) -> {
+                view.getNavigator().openDadosClienteView(currentCliente)
+            }
+            else -> {
+                Timber.e("Click de menu com titulo: ${menuItem.title} está sendo ignorado")
+            }
+        }
     }
 }
--- projeto_android/app/src/test/java/dev/alexferreira/ui/presenter/MainMenuPresenterTest.kt
+++ projeto_android/app/src/test/java/dev/alexferreira/ui/presenter/MainMenuPresenterTest.kt
@@ -1,16 +1,26 @@
 package dev.alexferreira.ui.presenter
 
-import dev.alexferreira.data.model.Cliente
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import dev.alexferreira.R
 import dev.alexferreira.ui.contract.MainMenuContract
+import dev.alexferreira.ui.model.MainMenuModel
 import org.junit.Test
 import org.mockito.Mockito
 
 class MainMenuPresenterTest :
-    AbstractPresenterTest<MainMenuContract.View, MainMenuContract.Presenter, MainMenuPresenter>() {
+    AbstractPresenterTest<MainMenuContract.View, MainMenuContract.Presenter, MainMenuPresenter>(
+        MainMenuPresenter::class.java,
+        MainMenuContract.View::class.java
+    ) {
 
     @Test
     fun contract_selectMenuItem_callView() {
-        contract.selectMenuItem()
+        val menuTitle = "Cliente"
+        presenter.currentCliente = mock()
+        Mockito.`when`(context.getString(any())).thenReturn(menuTitle)
+        val menuItem = MainMenuModel(R.drawable.ic_maxima_pessoa, menuTitle)
+        contract.selectMenuItem(menuItem)
 
         Mockito.verify(view).getNavigator()
         Mockito.verify(navigator).openDadosClienteView(any())
