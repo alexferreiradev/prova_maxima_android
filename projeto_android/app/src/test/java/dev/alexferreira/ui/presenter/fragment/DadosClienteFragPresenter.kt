2f436a50ece8fe6b94d14178a0b092be7975d891
Alex Rabelo Ferreira <arf92@live.com>
2019-09-15 10:43:03 -0300

    Adicao de dados cliente frag presenter

---
+++ projeto_android/app/src/test/java/dev/alexferreira/ui/presenter/fragment/DadosClienteFragPresenter.kt
@@ -0,0 +1,12 @@
+package dev.alexferreira.ui.presenter.fragment
+
+import dev.alexferreira.ui.contract.DadosClienteContract
+import javax.inject.Inject
+
+class DadosClienteFragPresenter @Inject constructor() :
+    AbstractFragPresenter<DadosClienteContract.DadosClienteContract.FragView>(),
+    DadosClienteContract.DadosClienteContract.FragPresenter {
+    override fun selectVerifyEstadoCliente() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ projeto_android/app/src/test/java/dev/alexferreira/ui/presenter/fragment/DadosClienteFragPresenterTest.kt
@@ -0,0 +1,10 @@
+package dev.alexferreira.ui.presenter.fragment
+
+import org.junit.Before
+
+class DadosClienteFragPresenterTest {
+
+    @Before
+    fun setUp() {
+    }
+}
