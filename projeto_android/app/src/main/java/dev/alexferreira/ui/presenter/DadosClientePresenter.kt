e2267971e3d0cd36144a845f5ec0023c997715c4
Alex Rabelo Ferreira <arf92@live.com>
2019-09-16 00:16:03 -0300

    Adicao de load cliente por id

--- projeto_android/app/src/main/java/dev/alexferreira/ui/presenter/DadosClientePresenter.kt
+++ projeto_android/app/src/main/java/dev/alexferreira/ui/presenter/DadosClientePresenter.kt
@@ -1,15 +1,21 @@
 package dev.alexferreira.ui.presenter
 
+import android.content.Intent
 import android.view.MenuItem
 import dev.alexferreira.R
+import dev.alexferreira.data.model.Cliente
+import dev.alexferreira.data.repository.IClienteRepository
 import dev.alexferreira.injection.scope.ActivityScope
 import dev.alexferreira.ui.adapter.DadosClienteViewPagerAdapter
 import dev.alexferreira.ui.contract.DadosClienteContract
+import dev.alexferreira.ui.task.LoadClienteTask
+import dev.alexferreira.ui.task.generic.TaskCallback
 import timber.log.Timber
 import javax.inject.Inject
 
 @ActivityScope
-class DadosClientePresenter @Inject constructor() : AbstractPresenter<DadosClienteContract.View>(),
+class DadosClientePresenter @Inject constructor(val cliRepo: IClienteRepository) :
+    AbstractPresenter<DadosClienteContract.View>(),
     DadosClienteContract.Presenter {
 
     override fun selectBottomNavItemMenu(menuItem: MenuItem): Boolean {
@@ -31,5 +37,19 @@ class DadosClientePresenter @Inject constructor() : AbstractPresenter<DadosClien
                 return false
             }
         }
+    }
+
+    override fun onViewStarted(intent: Intent?) {
+        super.onViewStarted(intent)
+        LoadClienteTask(object : TaskCallback<Cliente> {
+            override fun onEmpty() {
+            }
+
+            override fun onSuccessResult(model: Cliente) {
+                view.hideProgressBar()
+
+                view.initViewPager(model)
+            }
+        }, cliRepo).execute(intent!!.getStringExtra(getString(R.string.cliente_id_intent_key)))
     }
 }
